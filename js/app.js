5962b681369a1890384bc2bbf6f956ffa83a0332
Sumant Chhunchha <mr.chhunchha@gmail.com>
2016-04-04 00:34:32 -0700

    sample board

--- js/app.js
+++ js/app.js
@@ -1,32 +1,27 @@
-var app = angular.module("playsafewaymonopoly", ["firebase","ngRoute"]);
+
+
+
 var appURL = "https://playsafewaymonopoly.firebaseio.com";
 var ref = new Firebase(appURL);
 var userRef = new Firebase(appURL + "/users");
+var boardsRef = new Firebase(appURL + "/boards");
+var defaultBoardRef = new Firebase(appURL + "/default_board")
 
-app.config(['$routeProvider', '$locationProvider',function($routeProvider, $locationProvider){
-	$routeProvider
-	.when('/',{
-		controller: "AuthCtrl",
-		templateUrl: "/index.html"
-	}).
-	otherwise({
-		redirectTo: '/'
-	});
-}]);
-
-app.factory("Auth", function($firebaseAuth){
-	return $firebaseAuth(ref);
-});
-
-app.controller("AuthCtrl", function($scope, Auth, $location){
+PlayCntrl.$inject = ['$scope', 'Auth', '$location', '$firebaseObject'];
+function PlayCntrl($scope, Auth, $location, $firebaseObject){
 	$scope.provider = '';
 	$scope.authData;
+	$scope.userBoard;
+	$scope.nav  = { tabIndex : 1 };
+	$scope.user;
 
 	Auth.$onAuth(function(authData){
 		$scope.authData = authData;
 		if(authData) {
 			$scope.cachedProfile = getCachedProfile();
 			createUser();
+			$scope.creteBoardForUser();
+			getUserBoardData();
 			//$location.path("/authenticated");
 		}
 		console.log($scope.authData);
@@ -55,6 +50,7 @@ app.controller("AuthCtrl", function($scope, Auth, $location){
 			}
 			else {
 				console.log("User already exists");
+				$scope.user = $firebaseObject(userRef.child($scope.authData.uid));
 			}
 		});
 	}
@@ -63,7 +59,7 @@ app.controller("AuthCtrl", function($scope, Auth, $location){
 		if(!$scope.authData) return "";
 		switch($scope.authData.provider) {
 			case "google":
-			return $scope.authData.google.cachedUserProfile;
+				return $scope.authData.google.cachedUserProfile;
 			break;
 			default:
 			return "";
@@ -72,19 +68,64 @@ app.controller("AuthCtrl", function($scope, Auth, $location){
 
 	$scope.getUserImage = function() {
 		if(!$scope.authData) return "";
-
 		switch($scope.authData.provider) {
-			// case "github":
-			// 	return $scope.authData.github.cachedUserProfile.avatar_url ? $scope.authData.github.cachedUserProfile.avatar_url : "";
-			// 	break;
-			// case "facebook":
-			// 	return $scope.authData.facebook.profileImageURL ? $scope.authData.facebook.profileImageURL : "";
-			// 	break;
 			case "google":
-			return $scope.authData.google.profileImageURL ? $scope.authData.google.profileImageURL : "";
+				return $scope.authData.google.profileImageURL ? $scope.authData.google.profileImageURL : "";
 			break;
 			default:
 			return "";
 		}
 	}
+
+	// $scope.getDefaultBoard = function() {
+	// 	if($scope.authData) {
+	// 		defaultBoardRef.on("value", function(snapshot){
+	// 			console.log(snapshot.val());
+	// 		}, function(errorObject) {
+	// 			console.log("The read failed for default board");
+	// 		})
+	// 	}
+	// }
+
+	$scope.creteBoardForUser = function() {
+		boardsRef.on("value", function(snapshot){
+			if(snapshot.val() !== null) {
+				console.log("board exists for user");
+			} else {
+				defaultBoardRef.on("value", function(snapshot){
+					var data = snapshot.val();
+					boardsRef.child($scope.authData.uid).set({
+						data
+					});
+				}, function(errorObject) {
+					console.log("failed to read default board");
+				})
+			}
+		}, function(errorObject) {
+			console.log("failed to read board for user");
+		});
+	}
+
+	var getUserBoardData = function() {
+		$scope.userBoard = $firebaseObject(boardsRef.child($scope.authData.uid));
+	}
+};
+
+var app = angular.module("playsafewaymonopoly", ["firebase","ngRoute"]);
+
+app.config(['$routeProvider', '$locationProvider',function($routeProvider, $locationProvider){
+	$routeProvider
+	.when('/',{
+		controller: "AuthCtrl",
+		templateUrl: "/index.html"
+	}).
+	otherwise({
+		redirectTo: '/'
+	});
+}]);
+
+app.factory("Auth", function($firebaseAuth){
+	return $firebaseAuth(ref);
 });
+
+app.controller('playCntrl', PlayCntrl);

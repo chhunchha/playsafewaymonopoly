507d1f91f93d6fe0dd7cae2c41cc25b3af2c34ae
Sumant Chhunchha <mr.chhunchha@gmail.com>
2016-04-08 22:50:26 -0700

    real prizes and tickets

--- js/app.js
+++ js/app.js
@@ -279,7 +279,7 @@ function PlayCntrl($scope, Auth, $location, $firebaseObject, $http, $firebaseArr
 	$scope.searchText = "";
 };
 
-var app = angular.module("playsafewaymonopoly", ["firebase","ngRoute",'customFilter']);
+var app = angular.module("playsafewaymonopoly", ["firebase","ngRoute","customFilter","angular-toArrayFilter"]);
 
 app.config(['$routeProvider', '$locationProvider',function($routeProvider, $locationProvider){
 	$routeProvider
@@ -313,4 +313,23 @@ angular.module('customFilter',[])
     });
     return result;
   }
+});
+
+angular.module('angular-toArrayFilter', [])
+.filter('toArray', function () {
+  return function (obj, addKey) {
+    if (!angular.isObject(obj)) return obj;
+    if ( addKey === false ) {
+      return Object.keys(obj).map(function(key) {
+        return obj[key];
+      });
+    } else {
+      return Object.keys(obj).map(function (key) {
+        var value = obj[key];
+        return angular.isObject(value) ?
+          Object.defineProperty(value, '$key', { enumerable: false, value: key}) :
+          { $key: key, $value: value };
+      });
+    }
+  };
 });
